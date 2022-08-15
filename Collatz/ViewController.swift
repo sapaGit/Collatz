976c23552a451d75eedef8791b392bf9372b3fe8
sapaGit <mosevaly@gmail.com>
2022-08-15 23:37:54 +0300

    01.2 Add logic

--- Collatz/ViewController.swift
+++ Collatz/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderChosed(_ sender: UISlider) {
+        sender.minimumValue = 2
+        sender.maximumValue = 100
+        numberLabel.text = String(Int(sender.value))
+    }
+    @IBAction func submitTapped(_ sender: UIButton) {
+        var resultText = ""
+        guard var tempValue = Int(numberLabel.text ?? "Error") else { return }
+        while tempValue != 1 {
+            resultText += " \(tempValue)"
+            checkValue(value: &tempValue)
+        }
+        resultLabel.text = resultText + " 1"
+    }
+    func checkValue(value: inout Int) {
+        if value % 2 == 0 {
+            value = value/2
+        } else {
+            value = value * 3 + 1
+        }
+    }
 }
 
